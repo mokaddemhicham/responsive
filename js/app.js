602707ebe21da207aec401db9765cf376eed72b0
mokaddemhicham <73243340+mokaddemhicham@users.noreply.github.com>
2020-12-17 15:00:30 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function(){
+    'use_strict';
+    $('.classic-list li').click(function(){
+        $(this).addClass('selected').siblings('li').removeClass('selected');
+        $('.info div').hide();
+        $('.' + $(this).data('class')).fadeIn();
+    });
+});
