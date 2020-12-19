3249a6b800cc85021ac382374d975a77c5b481b6
mokaddemhicham <73243340+mokaddemhicham@users.noreply.github.com>
2020-12-19 23:33:27 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ $(function(){
         $('.' + $(this).data('class')).fadeIn();
     });
     $('.menu-toggle').click(function(){
-        $('nav ul').toggleClass('visible');
+        $('nav ul').toggleClass('visible-menu');
+        $('.menu-toggle span:first-of-type').toggleClass('span1');
+        $('.menu-toggle span:nth-of-type(2)').toggleClass('span2');
+        $('.menu-toggle span:last-of-type').toggleClass('span3');
     });
 });
